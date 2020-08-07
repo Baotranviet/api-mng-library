61388ec1d46f40c1c118f67b78791a3dc6a1c61b
baotranviet <vietbao148@gmail.com>
2020-08-07 08:22:55 +0700

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.use('/', (req, res) => {
+    res.json({"mess": "Hello World!"})
+})
+
+app.listen(PORT, () => {
+    console.log("Server started on http://localhost:" + PORT)
+})
+
+module.exports = app;
